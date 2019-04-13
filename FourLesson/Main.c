e890b0f502f978cc854d2b14e83e95f2113c7c78
WolfManR <wolfmanr@mail.ru>
2019-04-13 16:38:45 +0300

    Закончил канспект четвёртого урока

--- FourLesson/Main.c
+++ FourLesson/Main.c
@@ -2,4 +2,25 @@
 
 int main(int argc, const char* argv[]) {
 
+	int a = 10;
+	int b = 20;
+	int even = 0;
+	while (a < b) {
+		if (a % 2 == 0) even++;
+		a++;
+	}
+	printf("there are %d even numbers in sequence\n", even);
+
+	int input;
+	do {
+		printf("enter the divider for 100(remember, that you cannot divide by zero)\n");
+		scanf_s("%d", &input);
+	} while (input == 0);
+	printf("100 / %d = %d\n", input, 100 / input);
+	printf("and the reminder will be %d,by the way \n", 100 % input);
+
+
+
+
+	return 0;
 }
