577e46d9c5815c7a5d01ebb6ff777770b2efab65
WolfManR <wolfmanr@mail.ru>
2019-04-18 20:41:06 +0300

    добавил выборку заданий в класс main
    обнаруженные ошибки: getchar(); вызывается лишь один раз

--- Homeworks/LessonFive.c
+++ Homeworks/LessonFive.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int LessonFive_main() {
-
+	printf("This is lesson five homework!\n");
 	return 0;
 }
--- Homeworks/Source.c
+++ Homeworks/Source.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
 int main() {
-	char exit;
-	printf("tipe \"q\", if you wana exit");
+	printf("tipe \"q\", if you wana exit: ");
+	char exit = getchar();
+	while ( exit!= 'q') {
+		int lesson;
+		printf("Choose on which lesson you wana check homework work\nLessons:\n 1 : Five\n");
+		scanf_s("%d", &lesson);
+		switch (lesson)
+		{
+		case 1:
+			LessonFive_main();
+			printf("End Lesson Five Homework!\n");
+			break;
+		default:
+			printf("There no lesson with this number!\n");
+			break;
+		}
 
-	int lesson;
-	printf("Chose on which lesson you wana check homework work\nLessons:\n 1 : Five\n");
-	scanf_s("%d",&lesson );
-	switch (lesson)
-	{
-	case 1:
-		LessonFive_main();
-		break;
-	default:
-		break;
+		printf("\ntipe \"q\", if you wana exit: ");
+		exit=getchar();
 	}
+	
 	
 	return 0;
 }
