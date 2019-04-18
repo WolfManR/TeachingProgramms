a9e2065daed0a71c483b76a5e735e3299ef1c77c
WolfManR <wolfmanr@mail.ru>
2019-04-18 20:00:44 +0300

    настраиваю класс main для перебора заданий пользовательским вводом

--- Homeworks/Source.c
+++ Homeworks/Source.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
 int main() {
+	char exit;
+	printf("tipe \"q\", if you wana exit");
 
-
+	int lesson;
+	printf("Chose on which lesson you wana check homework work\nLessons:\n 1 : Five\n");
+	scanf_s("%d",&lesson );
+	switch (lesson)
+	{
+	case 1:
+		LessonFive_main();
+		break;
+	default:
+		break;
+	}
+	
 	return 0;
 }
