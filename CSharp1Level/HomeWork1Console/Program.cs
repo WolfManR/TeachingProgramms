87c23919b2ae4e1275efb68df25d161a2aa271d8
WolfManR <wolfmanr@mail.ru>
2019-07-26 16:45:06 +0300

    завершил изменение в форматированном выводе

--- CSharp1Level/HomeWork1Console/Program.cs
+++ CSharp1Level/HomeWork1Console/Program.cs
@@ -37,7 +37,7 @@ namespace HomeWork1Console
             Console.WriteLine(user.LastName + " " + user.FirstName + " " + user.Age + " лет, вес " + user.Weight + "кг, рост состовляет " + user.Height + "см");
             Console.WriteLine("Форматированный вывод");
             Console.Write("Это вы: ");
-            Console.WriteLine("{0} {1} {2:0 лет}, вес {3}кг, рост состовляет {4:N0}см", user.LastName,user.FirstName,user.Age,user.Weight,user.Height);
+            Console.WriteLine("{0} {1} {2:0 лет}, вес {3}кг, {4:рост состовляет 0,0}см", user.LastName,user.FirstName,user.Age,user.Weight,user.Height);
             Console.WriteLine("Интерполяция");
             Console.Write("Это вы: ");
             Console.WriteLine($"{user.LastName} {user.FirstName} {user.Age} лет, вес {user.Weight}кг, рост состовляет {user.Height}см");
