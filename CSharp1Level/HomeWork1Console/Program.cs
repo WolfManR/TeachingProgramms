4af37819b0609152388ac1c2270c0cedb6d71832
WolfManR <wolfmanr@mail.ru>
2019-07-26 14:43:42 +0300

    создал проект консоли .Net Framework для выполнения 1 ДЗ, добавил класс User, и начал создание метода вывода полной информации о пользователе по задаче 1

---
+++ CSharp1Level/HomeWork1Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        void WriteUserInfo(User user)
+        {
+            Console.WriteLine("Конкатенация");
+            Console.Write("Это вы: ");
+            Console.WriteLine(user.LastName + " " + user.FirstName + " " + user.Age + " лет, вес " + user.Weight + "кг, рост состовляет " + user.Height + "см");
+
+        }
+    }
+
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+    }
+}
