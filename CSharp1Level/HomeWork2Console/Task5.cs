98650c86d8cc6ec9441294615be2f4c7f1306e68
WolfManR <wolfmanr@mail.ru>
2019-07-30 23:31:29 +0300

    разрабатываю код задачи 5, добавил в комментариях варианты ИМТ и что они значат

--- CSharp1Level/HomeWork2Console/Task5.cs
+++ CSharp1Level/HomeWork2Console/Task5.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using HomeWorkLib;
+using HomeWorkLib.ConsoleWork;
 
 namespace HomeWork2Console
 {
     public class Task5 : ITaskWork
     {
+//        Индекс массы тела
+//        Соответствие между массой человека и его ростом 
+//              16 и менее            Выраженный дефицит массы тела 
+//              16—18,5               Недостаточная(дефицит) масса тела 
+//              18,5—24,99            Норма 
+//              25—30                 Избыточная масса тела(предожирение)
+//              30—35                 Ожирение 
+//              35—40                 Ожирение резкое 
+//              40 и более            Очень резкое ожирение
+        const double normalIMT = 18.6;
         public void Work()
         {
-            throw new NotImplementedException();
+            Console.Write("Пожалуйста введите вес в кг измеряемого человека ");
+            int weight=Helper.ReadInt();
+            Console.Write("Пожалуйста введите рост в метрах измеряемого человека ");
+            double height=double.Parse(Console.ReadLine());
+            double imt = Helper.IMT(weight,height);
         }
     }
 }
