5f5122ac0f67c63b29af272dcefded4d9cfd0fbf
WolfManR <wolfmanr@mail.ru>
2019-07-30 22:50:48 +0300

    добавил метод отображения меню

--- CSharp1Level/HomeWork2Console/Program.cs
+++ CSharp1Level/HomeWork2Console/Program.cs
@@ -18,7 +18,18 @@ namespace HomeWork2Console
         }
 
         
-
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("1. Task 1");
+            Console.WriteLine("2. Task 2");
+            Console.WriteLine("3. Task 3");
+            Console.WriteLine("4. Task 4");
+            Console.WriteLine("5. Task 5");
+            Console.WriteLine("6. Task 6");
+            Console.WriteLine("7. Task 7");
+            Console.WriteLine("0. Exit");
+        }
         
 
         
