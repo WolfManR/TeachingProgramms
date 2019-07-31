214574a18ae813900be4018b677434551fa5e6ce
WolfManR <wolfmanr@mail.ru>
2019-07-31 20:17:13 +0300

    добавил базовый класс для задач

---
+++ CSharp1Level/HomeWorkLib/ConsoleWork/HomeWorkTaskDecorate.cs
@@ -0,0 +1,14 @@
+﻿using HomeWorkLib.ConsoleWork;
+
+namespace HomeWorkLib
+{
+    public partial class HomeWorkTask
+    {
+        public void DecorativeWorkInConsole()
+        {
+            Helper.TaskTitle(TaskNumber);
+            Work();
+            Helper.TaskEnded(TaskNumber);
+        }
+    }
+}
---
+++ CSharp1Level/HomeWorkLib/HomeWorkTask.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeWorkLib
+{
+    public abstract partial class HomeWorkTask : ITaskWork
+    {
+        public string Title { get; }
+        public int TaskNumber { get; }
+        public string ToDo { get; }
+
+        public abstract void Work();
+    }
+}
