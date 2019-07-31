599304637116af4a98d21f3996f63c7267a7f545
WolfManR <wolfmanr@mail.ru>
2019-07-31 19:43:38 +0300

    перенёс текст кода 6 задачи разработанный на работе

--- CSharp1Level/HomeWork2Console/Task6.cs
+++ CSharp1Level/HomeWork2Console/Task6.cs
@@ -5,9 +5,21 @@ namespace HomeWork2Console
 {
     public class Task6 : ITaskWork
     {
+        const int startNumber = 1;
+        const int endNumber = 1_000_000_000;
         public void Work()
         {
-            throw new NotImplementedException();
+            string iInString;
+            int counter = 0;
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                iInString = i.ToString();
+                int delNumber = 0;
+                foreach (var item in iInString) delNumber += int.Parse(item.ToString());
+                if (i % delNumber == 0) counter++;
+                if (i % 500000 == 0) Console.WriteLine("Working " + i + "  " + counter);
+            }
+            Console.WriteLine("хороших чисел " + counter);
         }
     }
 }
