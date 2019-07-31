82a565bf6345e75f1adbfc8dc9b82cca1e6d78bd
WolfManR <wolfmanr@mail.ru>
2019-07-31 19:45:12 +0300

    перенёс текст кода 7 задачи разработанный на работе

--- CSharp1Level/HomeWork2Console/Task7.cs
+++ CSharp1Level/HomeWork2Console/Task7.cs
@@ -7,7 +7,45 @@ namespace HomeWork2Console
     {
         public void Work()
         {
-            throw new NotImplementedException();
+            int a, b;
+            do
+            {
+                Console.WriteLine("Введите два числа, одно должно быть меньше другого");
+                Console.WriteLine("введите первое число");
+                a = int.Parse(Console.ReadLine());
+                Console.WriteLine("введите второе число");
+                b = int.Parse(Console.ReadLine());
+                if (a == b)
+                    Console.WriteLine("числа не должны быть равны");
+            } while (a == b);
+
+            if (a > b)
+            {
+                printBetweenNumbers(b, a);
+                Console.WriteLine("\nсумма чисел равна " + sumBetweenNumbers(b, a));
+            }
+            else
+            {
+                printBetweenNumbers(a, b);
+                Console.WriteLine("\nсумма чисел равна " + sumBetweenNumbers(a, b));
+            }
+        }
+        void printBetweenNumbers(int a, int b)
+        {
+            a++;
+            if (a == b) return;
+            else
+            {
+                Console.Write(a + " ");
+                printBetweenNumbers(a, b);
+            }
+        }
+
+        int sumBetweenNumbers(int a, int b)
+        {
+            a++;
+            if (a == b) return 0;
+            else return a + sumBetweenNumbers(a, b);
         }
     }
 }
