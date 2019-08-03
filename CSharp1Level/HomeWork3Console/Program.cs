c3dc2603f6453d34c8a98c349e464986ffd0cefb
WolfManR <wolfmanr@mail.ru>
2019-08-03 16:51:16 +0300

    добавил в проект ДЗ 3 код меню из ДЗ 2

--- CSharp1Level/HomeWork3Console/Program.cs
+++ CSharp1Level/HomeWork3Console/Program.cs
@@ -1,9 +1,51 @@
-﻿namespace HomeWork3Console
+﻿using System;
+using HomeWorkLib;
+using HomeWorkLib.ConsoleWork;
+
+namespace HomeWork3Console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            HomeWorkTask[] tasks = { new Task1(), new Task2(), new Task3()};
+            string selectedMenuMember;
+            do
+            {
+                Menu(tasks);
+                selectedMenuMember = Console.ReadLine();
+                switch (selectedMenuMember)
+                {
+                    case "1":
+                        tasks[0].DecorativeWorkInConsole();
+                        break;
+                    case "2":
+                        tasks[1].DecorativeWorkInConsole();
+                        break;
+                    case "3":
+                        tasks[2].DecorativeWorkInConsole();
+                        break;
+                    case "0":
+                        Console.WriteLine("\nДо свидания");
+                        break;
+                    default:
+                        Console.WriteLine("\nУ нас нет задачи с таким номером, проверьте меню и введите команду ещё раз");
+                        break;
+                }
+            } while (selectedMenuMember != "0");
+
+            Helper.ProgramEnded();
+        }
+        static void Menu(HomeWorkTask[] tasks)
+        {
+            Console.Clear();
+            //int lastTaskNumber = 0;
+            foreach (var task in tasks)
+            {
+                Console.WriteLine($"{task.TaskNumber}. {task.Title}");
+                //lastTaskNumber = task.TaskNumber;
+            }
+            Console.WriteLine("0. Exit");
         }
     }
 }
