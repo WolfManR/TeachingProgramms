7cd326ca0fda3e684e7d68739e976f0673512287
WolfManR <wolfmanr@mail.ru>
2019-08-04 16:44:05 +0300

    создал класс дроби

--- CSharp1Level/HomeWork3Console/Task3.cs
+++ CSharp1Level/HomeWork3Console/Task3.cs
@@ -14,11 +14,78 @@ namespace HomeWork3Console
                                        "\nНаписать программу, демонстрирующую все разработанные элементы класса." +
                                        "\n\n**Добавить проверку, чтобы знаменатель не равнялся 0. " +
                                        "\nВыбрасывать исключение ArgumentException(\"Знаменатель не может быть равен 0\");" +
-                                       "\nДобавить упрощение дробей.";
+                                       "\n\nДобавить упрощение дробей.";
 
         public override void Work()
         {
             throw new NotImplementedException();
+        }
+
+        public class Fraction
+        {
+            private int denominator;
+            public int Hole { get; set; } = 0;
+            public int Numerator { get; set; } = 0;
+            public int Denominator
+            {
+                get => denominator;
+                set
+                {
+                    if (value != 0)
+                        denominator = value;
+                    else throw new ArgumentException("Знаменатель не может быть равен 0");
+                }
+            } 
+
+            static void LeadToACommonDenominator(ref Fraction left,ref Fraction right)
+            {
+                left.Numerator *= right.Denominator;
+                int temp = left.Denominator;
+                left.Denominator *= right.Denominator;
+                right.Numerator *= temp;
+                right.Denominator *= temp;
+            }
+
+            public override string ToString()
+            {
+                return (Hole==0)?$"{Numerator}/{Denominator}":$"{Hole} {Numerator}/{Denominator}";
+            }
+
+            public static Fraction operator +(Fraction left, Fraction right)
+            {
+                LeadToACommonDenominator(ref left, ref right);
+                return new Fraction() {Numerator=left.Numerator+right.Numerator,Denominator=left.Denominator};
+            }
+            public static Fraction operator -(Fraction left, Fraction right)
+            {
+                LeadToACommonDenominator(ref left, ref right);
+                return new Fraction() { Numerator = left.Numerator - right.Numerator, Denominator = left.Denominator };
+            }
+            public static Fraction operator *(Fraction left, Fraction right)
+            {
+                return new Fraction() { Numerator = left.Numerator * right.Numerator, Denominator = left.Denominator * right.Denominator};
+            }
+            public static Fraction operator *(Fraction left, int number)
+            {
+                return new Fraction() { Numerator = left.Numerator * number, Denominator = left.Denominator };
+            }
+            public static Fraction operator /(Fraction left, Fraction right)
+            {
+                return new Fraction() { Numerator = left.Numerator * right.Denominator, Denominator = left.Denominator * right.Numerator };
+            }
+            public static void SimplerFraction(ref Fraction fraction)
+            {
+                if(fraction.Numerator%fraction.Denominator==0)
+                {
+                    fraction.Hole += fraction.Numerator / fraction.Denominator;
+                    fraction.Numerator = 0;
+                }
+                else
+                {
+                    fraction.Hole += fraction.Numerator / fraction.Denominator;
+                    fraction.Numerator %= fraction.Denominator;
+                }
+            }
         }
     }
 }
