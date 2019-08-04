8d526523ed90ba681b42668ec01dc7a6cc1ef852
WolfManR <wolfmanr@mail.ru>
2019-08-04 22:10:41 +0300

    сделал дохохо всего и забыл делать комиты, бяда

--- CSharp1Level/HomeWork3Console/Task3.cs
+++ CSharp1Level/HomeWork3Console/Task3.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using HomeWorkLib;
+using HomeWorkLib.ConsoleWork;
 
 namespace HomeWork3Console
 {
@@ -18,7 +19,39 @@ namespace HomeWork3Console
 
         public override void Work()
         {
-            throw new NotImplementedException();
+            
+            Fraction first = new Fraction() { Numerator = -2, Denominator = 5 };
+            Fraction second = new Fraction() { Numerator = 1, Denominator = 4 };
+            int num = 6;
+            Console.WriteLine("Вы будете вводить свой вариант дробей? \n<2 - да\n>=2 - нет");
+            if (Helper.ReadInt() < 2)
+            {
+                first = GetFractionFromUserInput("Первая дробь");
+                second = GetFractionFromUserInput("Вторая дробь");
+                num = int.Parse(Helper.GetValueInMsgLine("введите целое число для умножения на него первой дроби"));
+            }
+            
+            
+            PrintResults(first, second, num);
+        }
+
+        Fraction GetFractionFromUserInput(string whichFraction)
+        {
+            var fraction=new Fraction();
+            Console.WriteLine(whichFraction);
+            fraction.Hole = int.Parse(Helper.GetValueInMsgLine("Введите целое число"));
+            fraction.Numerator = int.Parse(Helper.GetValueInMsgLine("Введите числитель"));
+            fraction.Denominator = int.Parse(Helper.GetValueInMsgLine("Введите знаменатель"));
+            return fraction;
+        }
+        void PrintResults(Fraction first,Fraction second, int number)
+        {
+            Console.WriteLine();
+            Console.WriteLine("сумма " + (first + second).ToString());
+            Console.WriteLine("вычитание " + (first - second).ToString());
+            Console.WriteLine("умножение " + (first * second).ToString());
+            Console.WriteLine("умножение на целое " + (first * number).ToString());
+            Console.WriteLine("деление " + (first / second).ToString());
         }
 
         public class Fraction
@@ -37,16 +70,45 @@ namespace HomeWork3Console
                 }
             } 
 
-            public static void LeadToACommonDenominator(ref Fraction left,ref Fraction right)
+            public static void LeadToACommonDenominator(Fraction left,Fraction right)
             {
-                left.Numerator *= right.Denominator;
-                int temp = left.Denominator;
-                left.Denominator *= right.Denominator;
-                right.Numerator *= temp;
-                right.Denominator *= temp;
+                if (left.Denominator != right.Denominator)
+                {
+                    left.Numerator *= right.Denominator;
+                    int temp = left.Denominator;
+                    left.Denominator *= right.Denominator;
+                    right.Numerator *= temp;
+                    right.Denominator *= temp;
+                }
             }
             public static void SimplerFraction(ref Fraction fraction)
             {
+                //Убираю десятки
+                while (fraction.Numerator / 10 != 0 & fraction.Denominator / 10 != 0 &fraction.Numerator%10==0&fraction.Denominator%10==0)
+                {
+                    fraction.Numerator /= 10;
+                    fraction.Denominator /= 10;
+                }
+                //Проверяю делится ли числитель и знаменатель на друг друга
+                if(fraction.Denominator%fraction.Numerator==0)
+                {
+                    fraction.Denominator /= fraction.Numerator;
+                    fraction.Numerator /= fraction.Numerator;
+                }else if (fraction.Numerator % fraction.Denominator == 0)
+                {
+                    fraction.Numerator /= fraction.Denominator;
+                    fraction.Denominator /= fraction.Denominator;
+                }
+                //Убираю кратность?
+                for (int i = 9; i > 1; i--)
+                {
+                    if (fraction.Numerator % i == 0 & fraction.Denominator % i == 0)
+                    {
+                        fraction.Numerator /= i;
+                        fraction.Denominator /= i;
+                    }
+                }
+                //Выделяю целое и передаю ему знак дроби
                 if(fraction.Numerator%fraction.Denominator==0)
                 {
                     fraction.Hole += fraction.Numerator / fraction.Denominator;
@@ -55,7 +117,17 @@ namespace HomeWork3Console
                 else
                 {
                     fraction.Hole += fraction.Numerator / fraction.Denominator;
-                    fraction.Numerator %= fraction.Denominator;
+                    var newNumerator = fraction.Numerator % fraction.Denominator;
+
+                    if (newNumerator < 0&fraction.Hole>0)
+                    {
+                        fraction.Numerator = newNumerator * -1;
+                        fraction.Hole *= -1;
+                    }
+                    else
+                    {
+                        fraction.Numerator= newNumerator;
+                    }
                 }
             }
 
@@ -67,25 +139,29 @@ namespace HomeWork3Console
             #endregion
 
             #region Operator's
-            public static Fraction operator +(Fraction left, Fraction right)
+            public static Fraction operator +(Fraction Left, Fraction Right)
             {
+                var left = new Fraction() { Hole=Left.Hole,Numerator=Left.Numerator,Denominator=Left.Denominator};
+                var right = new Fraction() { Hole = Right.Hole, Numerator = Right.Numerator, Denominator = Right.Denominator };
                 left.Numerator += left.Denominator * left.Hole;
                 right.Numerator += right.Denominator * right.Hole;
-                LeadToACommonDenominator(ref left, ref right);
-                return new Fraction() { Numerator = left.Numerator + right.Numerator, Denominator = left.Denominator };
+                LeadToACommonDenominator(left, right);
+                return new Fraction() { Numerator = left.Numerator + right.Numerator, Denominator = left.Denominator }; ;
             }
-            public static Fraction operator -(Fraction left, Fraction right)
+            public static Fraction operator -(Fraction Left, Fraction Right)
             {
+                var left = new Fraction() { Hole = Left.Hole, Numerator = Left.Numerator, Denominator = Left.Denominator };
+                var right = new Fraction() { Hole = Right.Hole, Numerator = Right.Numerator, Denominator = Right.Denominator };
                 left.Numerator += left.Denominator * left.Hole;
                 right.Numerator += right.Denominator * right.Hole;
-                LeadToACommonDenominator(ref left, ref right);
-                return new Fraction() { Numerator = left.Numerator - right.Numerator, Denominator = left.Denominator };
+                LeadToACommonDenominator(left, right);
+                return new Fraction() { Numerator = left.Numerator - right.Numerator, Denominator = left.Denominator }; ;
             }
             public static Fraction operator *(Fraction left, Fraction right)
             {
                 left.Numerator += left.Denominator * left.Hole;
                 right.Numerator += right.Denominator * right.Hole;
-                return new Fraction() { Numerator = left.Numerator * right.Numerator, Denominator = left.Denominator * right.Denominator };
+                return new Fraction() { Numerator = left.Numerator * right.Numerator, Denominator = left.Denominator * right.Denominator }; ;
             }
             public static Fraction operator *(Fraction left, int number)
             {
