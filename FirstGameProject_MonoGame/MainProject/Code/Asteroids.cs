dc7545a0b8591b765ee68ed22f20fac2eab3624d
Ivan <wolfmanr@mail.ru>
2019-08-10 16:48:52 +0300

    добавил классы Asteroid и Star

---
+++ FirstGameProject_MonoGame/MainProject/Code/Asteroids.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace MainProject
+{
+    public class Asteroids
+    {
+        public static int Width, Height;
+        public static Random rnd = new Random();
+        public static SpriteBatch SpriteBatch { get; set; }
+        static Star[] stars;
+        public static int GetIntRnd(int min, int max) => rnd.Next(min, max);
+        public static void Init(SpriteBatch spriteBatch,int width,int height)
+        {
+            Width = width;
+            Height = height;
+            SpriteBatch = SpriteBatch;
+            stars = new Star[50];
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = new Star(new Vector2(-rnd.Next(1, 10), 0));
+            }
+        }
+        public static void Draw()
+        {
+            foreach (Star star in stars)
+            {
+                star.Draw();
+            }
+        }
+        public static void Update()
+        {
+            foreach (Star star in stars)
+            {
+                star.Update();
+            }
+        }
+    }
+}
---
+++ FirstGameProject_MonoGame/MainProject/Code/Star.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MainProject
+{
+    public class Star
+    {
+        Vector2 Pos;
+        Vector2 Dir;
+        Color color;
+
+        public static Texture2D Texture2D { get; set; }
+
+        public Star(Vector2 pos,Vector2 dir)
+        {
+            Pos = pos;
+            Dir = dir;
+        }
+
+        public Star(Vector2 dir)
+        {
+            Dir = dir;
+            RandomSet();
+        }
+
+        public void Update()
+        {
+            Pos += Dir;
+            if (Pos.X<0)
+            {
+                RandomSet();
+            }
+        }
+
+        public void RandomSet()
+        {
+            Pos = new Vector2
+                (
+                Asteroids.GetIntRnd(Asteroids.Width, Asteroids.Width + 300),
+                Asteroids.GetIntRnd(0, Asteroids.Height)
+                );
+            color = Color.FromNonPremultiplied
+                (
+                Asteroids.GetIntRnd(0, 256),
+                Asteroids.GetIntRnd(0, 256),
+                Asteroids.GetIntRnd(0, 256),
+                Asteroids.GetIntRnd(0, 256)
+                );
+        }
+
+        public void Draw() => Asteroids.SpriteBatch.Draw(Texture2D, Pos, color);
+    }
+}
