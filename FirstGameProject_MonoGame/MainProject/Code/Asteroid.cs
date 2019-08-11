f546da859f96d84101ccc1f8192d60f4fff0d040
WolfManR <wolfmanr@mail.ru>
2019-08-11 23:26:27 +0300

    добавил класс объекта астероида

---
+++ FirstGameProject_MonoGame/MainProject/Code/Asteroid.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MainProject.Code
+{
+    public class Asteroid
+    {
+        Vector2 Pos, Dir;
+        Color color=Color.White;
+        public Texture2D Texture { get; set; }
+
+        public Asteroid(Vector2 pos,Vector2 dir)
+        {
+            Pos = pos;
+            Dir = dir;
+        }
+    }
+}
