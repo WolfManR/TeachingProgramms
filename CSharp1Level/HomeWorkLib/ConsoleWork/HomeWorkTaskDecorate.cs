1e48d50aa13121c221f7fba5b3bbcd8b6d275d25
Ivan <wolfmanr@mail.ru>
2019-08-13 21:49:18 +0300

    делегаты и удаление ненужных директив заменил работу метода декоративной работы в консоли на поддержку делегата и удалил не нужные директивы в некоторых объектах

--- CSharp1Level/HomeWorkLib/ConsoleWork/HomeWorkTaskDecorate.cs
+++ CSharp1Level/HomeWorkLib/ConsoleWork/HomeWorkTaskDecorate.cs
@@ -1,31 +1,30 @@
 ﻿using System;
-using HomeWorkLib.ConsoleWork;
 
 namespace HomeWorkLib
 {
     public partial class HomeWorkTask
     {
         public delegate void ConsoleColoredMsg(string msg, ConsoleColor BackColor, ConsoleColor ForeColor);
-        void TaskTitle(int taskNumber)
+        ConsoleColoredMsg Del;
+        void PrintColoredMsg(string msg, ConsoleColor BackColor, ConsoleColor ForeColor)
         {
-            Helper.PrintColoredMsg($"\nЗадача {taskNumber}", ConsoleColor.DarkBlue, ConsoleColor.Yellow);
-        }
-        void PrintToDo(string ToDo)
-        {
-           Helper.PrintColoredMsg(ToDo + "\n", ConsoleColor.Gray, ConsoleColor.DarkMagenta);
-        }
-        void TaskEnded(int taskNumber) //в будущем заменить событием
-        {
-            Helper.PrintColoredMsg($"\nЗадача {taskNumber} завершила свою работу.\nНажмите любую кнопку для продолжения работы основной программы.", ConsoleColor.White, ConsoleColor.Black);
-            Console.ReadKey();
+            var defaultBack = Console.BackgroundColor;
+            var defaultFore = Console.ForegroundColor;
+            Console.BackgroundColor = BackColor;
+            Console.ForegroundColor = ForeColor;
+            Console.WriteLine($"\n{msg}\n");
+            Console.BackgroundColor = defaultBack;
+            Console.ForegroundColor = defaultFore;
         }
         public void DecorativeWorkInConsole()
         {
             Console.Clear();
-            TaskTitle(TaskNumber);
-            PrintToDo(ToDo);
+            Del = new ConsoleColoredMsg(PrintColoredMsg);
+            Del?.Invoke($"\nЗадача {TaskNumber}", ConsoleColor.DarkBlue, ConsoleColor.Yellow);
+            Del?.Invoke(ToDo + "\n", ConsoleColor.Gray, ConsoleColor.DarkMagenta);
             Work();
-            TaskEnded(TaskNumber);
+            Del?.Invoke($"\nЗадача {TaskNumber} завершила свою работу.\nНажмите любую кнопку для продолжения работы основной программы.", ConsoleColor.White, ConsoleColor.Black);
+            Console.ReadKey();
         }
     }
 }
