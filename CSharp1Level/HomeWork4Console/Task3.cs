235900ddd09866017c78f7d72d8db1520a1832f8
Ivan <wolfmanr@mail.ru>
2019-08-15 15:46:39 +0300

    перенёс класс одномерного массива в библиотеку HomeWorkLib

--- CSharp1Level/HomeWork4Console/Task3.cs
+++ CSharp1Level/HomeWork4Console/Task3.cs
@@ -23,63 +23,6 @@ namespace HomeWork4Console
             throw new System.NotImplementedException();
         }
 
-        public class OneRankArray
-        {
-            int[] array;
-            private int sum;
-
-            public int Length => array.Length;
-            public int Sum
-            {
-                get
-                {
-                    sum = 0;
-                    foreach (var item in array) sum += item;
-                    return sum;
-                }
-            }
-            public int this[int index] { get => array[index]; set => array[index] = value; }
-            public int MaxCount
-            {
-                get
-                {
-                    int count = 0;
-                    int max = array[0];
-                    for (int i = 1; i < Length; i++) if (array[i] > max) max = array[i];
-                    foreach (var item in array) if (item == max) count++;
-                    return count;
-                }
-            }
-
-            public OneRankArray(uint size, int minValue, int step)
-            {
-                array = new int[size];
-                for (int j = 0, i = minValue; j < Length; j++, i += step) array[j] = i;
-            }
-
-            public int[] Inverse()
-            {
-                int[] arr = new int[Length];
-                for (int i = 0; i < Length; i++) arr[i] = array[i] * -1;
-                return arr;
-            }
-
-            public void Multi(int number)
-            {
-                for (int i = 0; i < Length; i++) array[i] *= number;
-            }
-
-            public Dictionary<int,int> CountSameElements()
-            {
-                Dictionary<int, int> result = new Dictionary<int, int>();
-                result.Add(array[0], 1);
-                for (int i = 1; i < Length; i++)
-                {
-                    if (result.ContainsKey(array[i])) result[array[i]]++;
-                    else result.Add(array[i], 1);
-                }
-                return result;
-            }
-        }
+        
     }
 }
