a89980e05eae5654c6e3d2745cd3ce38b638e2ac
Ivan <wolfmanr@mail.ru>
2019-08-15 12:42:22 +0300

    создал класс одмерного массива для задачи 1

--- CSharp1Level/HomeWork4Console/Task3.cs
+++ CSharp1Level/HomeWork4Console/Task3.cs
@@ -1,4 +1,5 @@
 ﻿using HomeWorkLib;
+using System.Collections.Generic;
 
 namespace HomeWork4Console
 {
@@ -14,12 +15,71 @@ namespace HomeWork4Console
                                        "\nметод Inverse, возвращающий новый массив с измененными знаками у всех элементов массива(старый массив, остается без изменений)," +
                                        "\nметод Multi, умножающий каждый элемент массива на определённое число," +
                                        "\nсвойство MaxCount, возвращающее количество максимальных элементов." +
-                                       "\n\nб)** Создать библиотеку содержащую класс для работы с массивом.Продемонстрировать работу библиотекие) " +
+                                       "\n\nб)** Создать библиотеку содержащую класс для работы с массивом. Продемонстрировать работу библиотеки." +
                                        "\n\n*** Подсчитать частоту вхождения каждого элемента в массив(коллекция Dictionary<int, int>)";
 
         public override void Work()
         {
             throw new System.NotImplementedException();
+        }
+
+        public class OneRankArray
+        {
+            int[] array;
+            private int sum;
+
+            public int Length => array.Length;
+            public int Sum
+            {
+                get
+                {
+                    sum = 0;
+                    foreach (var item in array) sum += item;
+                    return sum;
+                }
+            }
+            public int this[int index] { get => array[index]; set => array[index] = value; }
+            public int MaxCount
+            {
+                get
+                {
+                    int count = 0;
+                    int max = array[0];
+                    for (int i = 1; i < Length; i++) if (array[i] > max) max = array[i];
+                    foreach (var item in array) if (item == max) count++;
+                    return count;
+                }
+            }
+
+            public OneRankArray(uint size, int minValue, int step)
+            {
+                array = new int[size];
+                for (int j = 0, i = minValue; j < Length; j++, i += step) array[j] = i;
+            }
+
+            public int[] Inverse()
+            {
+                int[] arr = new int[Length];
+                for (int i = 0; i < Length; i++) arr[i] = array[i] * -1;
+                return arr;
+            }
+
+            public void Multi(int number)
+            {
+                for (int i = 0; i < Length; i++) array[i] *= number;
+            }
+
+            public Dictionary<int,int> CountSameElements()
+            {
+                Dictionary<int, int> result = new Dictionary<int, int>();
+                result.Add(array[0], 1);
+                for (int i = 1; i < Length; i++)
+                {
+                    if (result.ContainsKey(array[i])) result[array[i]]++;
+                    else result.Add(array[i], 1);
+                }
+                return result;
+            }
         }
     }
 }
