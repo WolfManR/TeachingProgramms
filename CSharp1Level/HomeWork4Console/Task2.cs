b465b12a6770e0ea6049dd34e8e8e980ea5c471e
Ivan <wolfmanr@mail.ru>
2019-08-15 14:50:25 +0300

    добавил статический метод для чтения массива из файла

--- CSharp1Level/HomeWork4Console/Task2.cs
+++ CSharp1Level/HomeWork4Console/Task2.cs
@@ -1,4 +1,7 @@
 ﻿using HomeWorkLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace HomeWork4Console
 {
@@ -7,10 +10,10 @@ namespace HomeWork4Console
         public override string Title => "StaticClass";
 
         public override int TaskNumber => 2;
-                                            // спасибо посмеялся
+        // спасибо посмеялся
         public override string ToDo => "Реализуйте задачу 1 в виде статического класса StaticClass;" +
                                        "\nа) Класс должен содержать статический метод, который принимает на вход массив и решает задачу 1;" +
-                                       "\nб) * Добавьте статический метод для считывания массива из текстового файла.Метод должен возвращать массив целых чисел;" +
+                                       "\nб) * Добавьте статический метод для считывания массива из текстового файла. Метод должен возвращать массив целых чисел;" +
                                        "\n\nв)** Добавьте обработку ситуации отсутствия файла на диске.";
 
         public override void Work()
@@ -24,6 +27,25 @@ namespace HomeWork4Console
             {
                 Task1 task = new Task1(array);
                 task.Work();
+            }
+
+            public static int[] ReadArrayFromTextFile(string filename)
+            {
+                int[] arr;
+                StreamReader sr = new StreamReader(filename);
+                List<int> list = new List<int>();
+                for (int i = 0; sr.EndOfStream; i++)
+                {
+                    list.Add(int.Parse(sr.ReadLine()));
+                }
+                sr?.Close();
+                arr = new int[list.Count];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = list[i];
+                }
+                return arr;
+
             }
         }
     }
