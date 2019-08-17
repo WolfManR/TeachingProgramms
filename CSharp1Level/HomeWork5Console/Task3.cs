f66c12f6b3e35a2b8f1ae5878d717315d7f58739
Ivan <wolfmanr@mail.ru>
2019-08-17 12:09:18 +0300

    интегрировал базовую конструкцию выполнения дз

---
+++ CSharp1Level/HomeWork5Console/Task3.cs
@@ -0,0 +1,18 @@
+﻿using HomeWorkLib;
+
+namespace HomeWork5Console
+{
+    internal class Task3 : HomeWorkTask
+    {
+        public override string Title => throw new System.NotImplementedException();
+
+        public override int TaskNumber => throw new System.NotImplementedException();
+
+        public override string ToDo => throw new System.NotImplementedException();
+
+        public override void Work()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
