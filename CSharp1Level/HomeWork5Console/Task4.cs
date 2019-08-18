4c0eb6e77965a5d3e90994ab9f27707af8d70c5f
Ivan <wolfmanr@mail.ru>
2019-08-18 18:39:39 +0300

    создал класс студент и структуру оценки

--- CSharp1Level/HomeWork5Console/Task4.cs
+++ CSharp1Level/HomeWork5Console/Task4.cs
@@ -1,4 +1,5 @@
 ﻿using HomeWorkLib;
+using System;
 
 namespace HomeWork5Console
 {
@@ -23,7 +24,72 @@ namespace HomeWork5Console
                                        "\n\nДостаточно решить 2 задачи. Старайтесь разбивать программы на подпрограммы. " +
                                        "\nПереписывайте в начало программы условие и свою фамилию. Все программы сделать в одном решении. " +
                                        "\nДля решения задач используйте неизменяемые строки (string)";
+        public struct Rating
+        {
+            public int First { get; }
+            public int Second { get; }
+            public int Third { get; }
+            public float AverageMark { get; }
+            public Rating(string[] ratings)
+            {
+                int[] marks = new int[3];
+                int counter = 0;
+                for (int i = 0; i < ratings.Length; i++)
+                    if (int.TryParse(ratings[i], out marks[i]))
+                    {
+                        if (marks[i] < 1 || marks[i] > 5) counter++;
+                    }
+                    else counter++;
+                if (counter > 0) throw new Exception("Неверные оценки");
+                First = marks[0];
+                Second = marks[1];
+                Third = marks[2];
+                AverageMark = (First + Second + Third) / 3.0f;
+            }
 
+            public override string ToString()
+            {
+                return $"{First} {Second} {Third}";
+            }
+        }
+        public class Student
+        {
+            private string firstName;
+            private string lastName;
+
+            public string LastName
+            {
+                get => lastName;
+                set
+                {
+                    if (value.Length > 20) throw new Exception("слишком длинная фамилия");
+                    lastName = value;
+                }
+            }
+            public string FirstName
+            {
+                get => firstName;
+                set
+                {
+                    if (value.Length > 15) throw new Exception("слишком длинное имя");
+                    firstName = value;
+                }
+            }
+            public Rating Ratings { get; set; }
+            public Student(string student)
+            {
+                string[] Info = student.Split(new char[] { ' ' });
+                if (Info.Length != 5) throw new Exception("Что-то пошло не так");
+                LastName = Info[0];
+                FirstName = Info[1];
+                Ratings = new Rating(new string[] { Info[2], Info[3], Info[4] });
+            }
+
+            public override string ToString()
+            {
+                return $"{LastName} {FirstName} {Ratings.ToString()}";
+            }
+        }
         public override void Work()
         {
             throw new System.NotImplementedException();
