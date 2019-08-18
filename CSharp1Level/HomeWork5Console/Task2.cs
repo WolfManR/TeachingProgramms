d15eb5f4cf38f95b02b04402fdf888438371803f
Ivan <wolfmanr@mail.ru>
2019-08-18 12:09:58 +0300

    завершил реализацию методов класса Message

--- CSharp1Level/HomeWork5Console/Task2.cs
+++ CSharp1Level/HomeWork5Console/Task2.cs
@@ -1,6 +1,7 @@
 ﻿using HomeWorkLib;
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace HomeWork5Console
 {
@@ -30,26 +31,40 @@ namespace HomeWork5Console
                 if (maxLength < 2) throw new Exception("Длинна слова не должна быть меньше 2");
                 List<string> list = new List<string>();
                 string[] words = SplitMsg(msg);
-                foreach (var item in words) if (item.Length <= maxLength & list.Contains(item)) list.Add(item);
+                foreach (var item in words) if (item.Length <= maxLength & !list.Contains(item)) list.Add(item);
                 return list.ToArray();
             }
 
-            public static string DeleteWordsWithSymbolOnEnd(string msg,char EndSymbol)
+            public static string[] DeleteWordsWithSymbolOnEnd(string msg,char EndSymbol)
             {
                 string[] words = SplitMsg(msg);
                 List<string> list = new List<string>();
                 foreach (var item in words) if (item[item.Length - 1] != EndSymbol) list.Add(item);
-                return list.ToArray().ToString();
+                return list.ToArray();
             }
 
-            public static void FindLongestWord(string msg)
+            public static string FindLongestWord(string msg)
             {
-
+                string[] words = SplitMsg(msg);
+                int indexMax = 0;
+                for (int i = 0; i < words.Length; i++) if (words[i].Length > words[indexMax].Length) indexMax = i;
+                return words[indexMax];
             }
 
-            public static void NewStringWithLongestWords(string msg)
+            public static string NewStringWithLongestWords(string msg)
             {
-
+                string[] words = SplitMsg(msg);
+                int maXLength = 0;
+                foreach (var item in words)if (item.Length > maXLength) maXLength = item.Length;
+                List<string> list = new List<string>();
+                foreach (var item in words) if (item.Length == maXLength & !list.Contains(item)) list.Add(item);
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (i != list.Count - 1) builder.Append(list[i] + " ");
+                    else builder.Append(list[i]);
+                }
+                return builder.ToString();
             }
         }
     }
