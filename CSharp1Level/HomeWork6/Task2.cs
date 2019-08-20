ecae9ddf17781c47b283d2540a693cbabaeb90f9
Ivan <wolfmanr@mail.ru>
2019-08-20 18:54:47 +0300

    добавил файлы учеников и копию программы нахождения минимума

--- CSharp1Level/HomeWork6/Task2.cs
+++ CSharp1Level/HomeWork6/Task2.cs
@@ -1,4 +1,6 @@
 ﻿using HomeWorkLib;
+using System;
+using System.IO;
 
 namespace HomeWork6
 {
@@ -12,10 +14,42 @@ namespace HomeWork6
                                        "\nа) Сделайте меню с различными функциями и предоставьте пользователю выбор, для какой функции и на каком отрезке находить минимум." +
                                        "\nб) Используйте массив(или список) делегатов, в котором хранятся различные функции." +
                                        "\nв) *Переделайте функцию Load, чтобы она возвращала массив считанных значений. Пусть она возвращает минимум через параметр.";
+        public static double F(double x) => x * x - 50 * x + 10;
+
+        public static void SaveFunc(string fileName, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(F(x));
+                x += h;// x=x+h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+        public static double Load(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            return min;
+        }
 
         public override void Work()
         {
-            throw new System.NotImplementedException();
+            SaveFunc("data.bin", -100, 100, 0.5);
+            Console.WriteLine(Load("data.bin"));
         }
     }
 }
