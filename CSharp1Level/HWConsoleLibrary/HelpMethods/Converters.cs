0a278e06b6936242f315783beddf497f84f35a5e
Ivan <wolfmanr@mail.ru>
2019-08-20 20:24:17 +0300

    выполнил задачу 1 и расширил класс Converter делегатом и переполнением ReadInt

--- CSharp1Level/HWConsoleLibrary/HelpMethods/Converters.cs
+++ CSharp1Level/HWConsoleLibrary/HelpMethods/Converters.cs
@@ -4,6 +4,9 @@ namespace HWConsoleLibrary
 {
     public static class Converters
     {
+        public delegate string GetMsg(string msg);
+        public static GetMsg funcMsg { get; set; }
         public static int ReadInt() => int.Parse(Console.ReadLine());
+        public static int ReadInt(string msg) => int.Parse(funcMsg?.Invoke(msg));
     }
 }
--- CSharp1Level/HomeWork6/Task1.cs
+++ CSharp1Level/HomeWork6/Task1.cs
@@ -1,4 +1,6 @@
 ﻿using HomeWorkLib;
+using HWConsoleLibrary;
+using System;
 
 namespace HomeWork6
 {
@@ -10,10 +12,22 @@ namespace HomeWork6
 
         public override string ToDo => "Изменить программу вывода функции так, чтобы можно было передавать функции типа double (double,double)." +
                                        "\nПродемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x)";
-
         public override void Work()
         {
-            throw new System.NotImplementedException();
+            Converters.funcMsg = ConsoleMsg.GetValueInMsgLine;
+            Func<double,double,double> function;
+
+            Console.WriteLine("Демонстрация взаимодействия делегата и функции с уравнением a*x^2");
+            double a = int.Parse("Введите значение переменной a");
+            double x = int.Parse("Введите значение переменной x");
+            function = (c, d) => c * Math.Pow(d, 2);
+            Console.WriteLine($"рельтат уравнения: {function?.Invoke(a, x)}");
+
+            Console.WriteLine("Демонстрация взаимодействия делегата и функции с уравнением a*sin(x)");
+            a = int.Parse("Введите значение переменной a");
+            x = int.Parse("Введите значение переменной x");
+            function = (c, d) => c * Math.Sin(d);
+            Console.WriteLine($"рельтат уравнения: {function?.Invoke(a, x)}");
         }
     }
 }
