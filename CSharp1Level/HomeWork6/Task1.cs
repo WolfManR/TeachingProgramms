ab78675154a8400a612e8cc92ddfc557a032a381
Ivan <wolfmanr@mail.ru>
2019-08-23 00:45:04 +0300

    перестроил первую задачу под дополненное описание самой задачи

--- CSharp1Level/HomeWork6/Task1.cs
+++ CSharp1Level/HomeWork6/Task1.cs
@@ -30,6 +30,23 @@ namespace HomeWork6
             }
             Console.WriteLine("---------------------");
         }
+        /// <summary>
+        /// выводит в консоль результат вычислений указанной функции
+        /// </summary>
+        /// <param name="F">уравнение графика</param>
+        /// <param name="a">константа</param>
+        /// <param name="x1">начальное значение</param>
+        /// <param name="x2">конечное значение</param>
+        public static void Table(Func<double, double, double> F, double a, double x1, double x2)
+        {
+            Console.WriteLine("----- X ----- Y -----");
+            while (x1 <= x2)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x1, F?.Invoke(a, x1));
+                x1 += 1;
+            }
+            Console.WriteLine("---------------------");
+        }
         // Создаем метод для передачи его в качестве параметра в Table
         public static double MyFunc(double x)
         {
@@ -49,24 +66,23 @@ namespace HomeWork6
             Table(Math.Sin, -2, 2);      // Можно передавать уже созданные методы
             Console.WriteLine("Таблица функции x^2:");
             // Упрощение(с C# 2.0). Использование анонимного метода
-            Table(delegate (double x) { return x * x; }, 0, 3);
-
+            Table(delegate (double c) { return c * c; }, 0, 3);
 
 
             Converters.funcMsg = ConsoleMsg.GetValueInMsgLine;
-            Func<double,double,double> function;
+            void PresentFunc(Func<double, double, double> func, string funcType)
+            {
+                Console.WriteLine($"\n\nДемонстрация взаимодействия делегата и функции с уравнением {funcType}");
+                double a = Converters.ReadInt("Введите значение переменной a");
+                double x1 = Converters.ReadInt("Введите начальное значение x");
+                double x2 = Converters.ReadInt("Введите конечное значение x");
+                Console.WriteLine($"Таблица уравнения {funcType}");
+                Table(func, a, x1, x2);
+            }
 
-            Console.WriteLine("Демонстрация взаимодействия делегата и функции с уравнением a*x^2");
-            double n = Converters.ReadInt("Введите значение переменной a");
-            double q = Converters.ReadInt("Введите значение переменной x");
-            function = (c, d) => c * Math.Pow(d, 2);
-            Console.WriteLine($"рельтат уравнения: {function?.Invoke(n, q)}");
+            PresentFunc((a, x) => a * x * x, "a*x^2");
 
-            Console.WriteLine("Демонстрация взаимодействия делегата и функции с уравнением a*sin(x)");
-            n = Converters.ReadInt("Введите значение переменной a");
-            q = Converters.ReadInt("Введите значение переменной x");
-            function = (c, d) => c * Math.Sin(d);
-            Console.WriteLine($"рельтат уравнения: {function?.Invoke(n, q)}");
+            PresentFunc((a, x) => a * Math.Sin(x), "a * sin(x)");
         }
     }
 }
