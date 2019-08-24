2d86cb49e3eccd213981fcfbf3703832b436db6c
Ivan <wolfmanr@mail.ru>
2019-08-24 21:16:21 +0300

    изменил расположение заголовка в Task1

--- CSharp1Level/HomeWork7/Task1.Designer.cs
+++ CSharp1Level/HomeWork7/Task1.Designer.cs
@@ -31,9 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.lblTitle = new System.Windows.Forms.Label();
             this.pHeader = new System.Windows.Forms.Panel();
-            this.btnBackToMain = new System.Windows.Forms.Button();
-            this.btnHelp = new System.Windows.Forms.Button();
             this.btnAppExit = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnBackToMain = new System.Windows.Forms.Button();
             this.tlTpHelp = new System.Windows.Forms.ToolTip(this.components);
             this.pHeader.SuspendLayout();
             this.SuspendLayout();
@@ -43,7 +43,7 @@
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
             this.lblTitle.ForeColor = System.Drawing.Color.DeepSkyBlue;
-            this.lblTitle.Location = new System.Drawing.Point(213, 9);
+            this.lblTitle.Location = new System.Drawing.Point(223, 9);
             this.lblTitle.Name = "lblTitle";
             this.lblTitle.Size = new System.Drawing.Size(279, 59);
             this.lblTitle.TabIndex = 0;
@@ -62,28 +62,6 @@
             this.pHeader.Size = new System.Drawing.Size(717, 81);
             this.pHeader.TabIndex = 1;
             // 
-            // btnBackToMain
-            // 
-            this.btnBackToMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.btnBackToMain.Location = new System.Drawing.Point(13, 12);
-            this.btnBackToMain.Name = "btnBackToMain";
-            this.btnBackToMain.Size = new System.Drawing.Size(33, 31);
-            this.btnBackToMain.TabIndex = 1;
-            this.btnBackToMain.Text = "<";
-            this.btnBackToMain.UseVisualStyleBackColor = true;
-            this.btnBackToMain.Click += new System.EventHandler(this.BtnBackToMain_Click);
-            // 
-            // btnHelp
-            // 
-            this.btnHelp.Enabled = false;
-            this.btnHelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.btnHelp.Location = new System.Drawing.Point(633, 12);
-            this.btnHelp.Name = "btnHelp";
-            this.btnHelp.Size = new System.Drawing.Size(33, 31);
-            this.btnHelp.TabIndex = 1;
-            this.btnHelp.Text = "?";
-            this.btnHelp.UseVisualStyleBackColor = true;
-            // 
             // btnAppExit
             // 
             this.btnAppExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
@@ -95,6 +73,34 @@
             this.btnAppExit.UseVisualStyleBackColor = true;
             this.btnAppExit.Click += new System.EventHandler(this.BtnAppExit_Click);
             // 
+            // btnHelp
+            // 
+            this.btnHelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnHelp.Location = new System.Drawing.Point(633, 12);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(33, 31);
+            this.btnHelp.TabIndex = 1;
+            this.btnHelp.Text = "?";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            // 
+            // btnBackToMain
+            // 
+            this.btnBackToMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnBackToMain.Location = new System.Drawing.Point(13, 12);
+            this.btnBackToMain.Name = "btnBackToMain";
+            this.btnBackToMain.Size = new System.Drawing.Size(33, 31);
+            this.btnBackToMain.TabIndex = 1;
+            this.btnBackToMain.Text = "<";
+            this.btnBackToMain.UseVisualStyleBackColor = true;
+            this.btnBackToMain.Click += new System.EventHandler(this.BtnBackToMain_Click);
+            // 
+            // tlTpHelp
+            // 
+            this.tlTpHelp.AutoPopDelay = 90000;
+            this.tlTpHelp.InitialDelay = 500;
+            this.tlTpHelp.IsBalloon = true;
+            this.tlTpHelp.ReshowDelay = 100;
+            // 
             // Task1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -103,7 +109,7 @@
             this.Controls.Add(this.pHeader);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Task1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Task1";
             this.pHeader.ResumeLayout(false);
             this.pHeader.PerformLayout();
