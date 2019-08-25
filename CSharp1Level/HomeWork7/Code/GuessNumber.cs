dbe4cd1ea58c879b9e78874ed98ca6f54a74fcac
Ivan <wolfmanr@mail.ru>
2019-08-25 15:35:26 +0300

    создал класс игры Угадай число и реализовал его

---
+++ CSharp1Level/HomeWork7/Code/GuessNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork7.Code
+{
+    public class GuessNumber
+    {
+        Random rand = new Random();
+        int tryes = 3;
+        int number;
+        int playersSteps = 0;
+        public int TryesLeft { get => tryes - playersSteps; }
+
+        public event EventHandler<GameStateEventArgs> Winned;
+        public event EventHandler<GameStateEventArgs> Loosed;
+        public GuessNumber()
+        {
+
+        }
+        public GuessNumber(int tryes)
+        {
+            this.tryes = tryes;
+        }
+        public void NewGame()
+        {
+            number = rand.Next(1, 100);
+            playersSteps = 0;
+        }
+
+        public void CheckNumber(int number)
+        {
+            if (number != this.number & tryes>playersSteps) return;
+            else if (number != this.number & tryes <= playersSteps) Loosed?.Invoke(this, new GameStateEventArgs($"Вы проиграли!"));
+            else Winned?.Invoke(this, new GameStateEventArgs($"Вы выйграли!"));
+        }
+    }
+}
