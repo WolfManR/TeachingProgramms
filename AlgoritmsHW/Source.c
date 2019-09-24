a945b811a1928d38d83f0703353a863dbd5eeb18
Ivan <wolfmanr@mail.ru>
2019-09-24 21:52:37 +0300

    скопировал команды для правильной работы консоли

---
+++ AlgoritmsHW/Source.c
@@ -0,0 +1,13 @@
+/*
+ * Source.c
+ *
+ *  Created on: 24 сент. 2019 г.
+ *      Author: wolfm
+ */
+#include <stdio.h>
+int main(){
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+	//setbuf(stdout, NULL);
+	//setbuf(stdin, NULL);
+}
