cd6bcf199fd49b352a9af67c2f8bcc40aff9630b
Ivan <wolfmanr@mail.ru>
2019-11-05 23:42:43 +0300

    добавил сортировку массива

--- CSharp2/ProjectWorkersConsole/Program.cs
+++ CSharp2/ProjectWorkersConsole/Program.cs
@@ -8,14 +8,22 @@ namespace ProjectWorkersConsole
         {
             try
             {
-                Workers workers = new Workers(
+                Worker[] array={
                     new FixPriceWorker { FirstName = "Aloy", LastName = "Fredecha", MonthySalary = 28000 },
-                    new HourPriceWorker { FirstName = "Josh", LastName = "Cayne", WorkDays = 22, HourlyRate = null },
+                    new HourPriceWorker { FirstName = "Josh", LastName = "Cayne", WorkDays = 22, HourlyRate = 100 },
                     new FixPriceWorker { FirstName = "Eddi", LastName = "Saidzem", MonthySalary = 35000 },
-                    new FixPriceWorker { FirstName = "Enny", LastName = "Caterty", MonthySalary = null },
-                    new HourPriceWorker { FirstName = "Billy", LastName = "Broderty", WorkDays = 15, HourlyRate = 82 }
-                );
+                    new FixPriceWorker { FirstName = "Enny", LastName = "Caterty", MonthySalary = 22000 },
+                    new HourPriceWorker { FirstName = "Billy", LastName = "Broderty", WorkDays = null, HourlyRate = 82 },
+                    new FixPriceWorker { FirstName = "Anna", LastName = "Caterty", MonthySalary = 24000 }
+                };
+                Workers workers = new Workers(array);
                 foreach (var item in workers)
+                {
+                    System.Console.WriteLine(item.ToString());
+                }
+                System.Console.WriteLine();
+                System.Array.Sort(array);
+                foreach (var item in array)
                 {
                     System.Console.WriteLine(item.ToString());
                 }
