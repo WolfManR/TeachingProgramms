f237dc0e32e95f3269b8b994b87e835ec41b7303
Ivan <wolfmanr@mail.ru>
2019-11-28 00:59:01 +0300

    добавил шаблон класса инициализатора БД

---
+++ CSharp2/OrganizationEF/EF/DBInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace OrganizationEF.EF
+{
+    public class DBInitializer: DropCreateDatabaseAlways<GBOrganizationEntities>
+    {
+        protected override void Seed(GBOrganizationEntities context)
+        {
+            base.Seed(context);
+        }
+    }
+}
