0e77fbc5634da0b316cf5006a2fd4514b5d0893c
Ivan <wolfmanr@mail.ru>
2019-11-28 00:56:15 +0300

    добавил строготипизированные классы репозиториев для Department и Employee

---
+++ CSharp2/OrganizationEF/Repos/DepartmentRepo.cs
@@ -0,0 +1,11 @@
+﻿using OrganizationEF.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrganizationEF.Repos
+{
+    public class DepartmentRepo:BaseRepo<Department>
+    {
+        public override List<Department> GetAll() => Context.Departments.ToList();
+    }
+}
---
+++ CSharp2/OrganizationEF/Repos/EmployeeRepo.cs
@@ -0,0 +1,11 @@
+﻿using OrganizationEF.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrganizationEF.Repos
+{
+    public class EmployeeRepo:BaseRepo<Employee>
+    {
+        public override List<Employee> GetAll() => Context.Employees.ToList();
+    }
+}
