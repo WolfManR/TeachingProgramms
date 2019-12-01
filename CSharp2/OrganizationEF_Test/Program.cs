03c5d8023b9282eb0b59fe4d9b1f46ad503aac78
Ivan <wolfmanr@mail.ru>
2019-12-02 00:58:32 +0300

    маленький апдейт тестовой версии EF в консоли

--- CSharp2/OrganizationEF_Test/Program.cs
+++ CSharp2/OrganizationEF_Test/Program.cs
@@ -13,19 +13,19 @@ namespace OrganizationEF_Test
             using(var context = new GBOrganizationEntities())
             {
                 foreach (Department item in context.Departments) Console.WriteLine($"department: {item.Name}, id: {item.Id}");
-            }
+            
             Console.WriteLine("\n\nRepos");
             Console.WriteLine("Department repo");
-            using(var depRepo= new DepartmentRepo())
+            using(var depRepo= new DepartmentRepo(context))
             {
                 foreach(Department item in depRepo.GetAll()) Console.WriteLine($"department: {item.Name,24}, id: {item.Id}");
             }
             Console.WriteLine("\n\nEmployee repo");
-            using (var emplRepo = new EmployeeRepo())
+            using (var emplRepo = new EmployeeRepo(context))
             {
                 foreach (Employee item in emplRepo.GetAll()) Console.WriteLine($"Employee: {item.FullName, 30}, id: {item.Id}, in department: {item.DepartmentId} {item.Department.Name}");
             }
-
+            }
             Console.ReadLine();
         }
     }
