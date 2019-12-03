edc38c6ca0d2e255d43d0e55b75cf25d697e004b
Ivan <wolfmanr@mail.ru>
2019-12-03 16:43:07 +0300

    добавил класс ADOBase и иинтерфейс IRepo

---
+++ CSharp2/OrganizationProject_ADO_Library/Repos/IRepo.cs
@@ -0,0 +1,22 @@
+﻿using OrganizationProject_ADO_Library.ADO;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OrganizationProject_ADO_Library.Repos
+{
+    public interface IRepo
+    {
+        ADOBase context { get; set; }
+        SqlCommand SelectAllCommand { get; set; }
+        SqlCommand SelectOneById { get; set; }
+        SqlCommand UpdateCommand { get; set; }
+        SqlCommand InsertCommand { get; set; }
+        SqlCommand DeleteCommand { get; set; }
+
+        void GetAll(DataTable table);
+        void GetOneById(int id);
+        void Insert(DataTable table);
+        void Update(DataTable table);
+        void Delete(DataTable table);
+    }
+}
