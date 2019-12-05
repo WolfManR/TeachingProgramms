1b60aa82a5508a69a5401351d8e7edb913611280
Ivan <wolfmanr@mail.ru>
2019-12-05 13:08:19 +0300

    добавил команды Employee

--- CSharp2/OrganizationProject_ADO_Library/Repos/EmployeeRepo.cs
+++ CSharp2/OrganizationProject_ADO_Library/Repos/EmployeeRepo.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OrganizationProject_ADO_Library.ADO;
 
 namespace OrganizationProject_ADO_Library.Repos
@@ -13,13 +8,17 @@ namespace OrganizationProject_ADO_Library.Repos
     {
         public EmployeeRepo(ADOBase context) : base(context)
         {
-            SelectAllCommand = new SqlCommand("Select * From Employees", context.Connection);
+            SelectAllCommand = new SqlCommand("Select Employee.Id,Employee.FirstName,Employee.LastName,Departments.Name From Employees Inner Join Departments On DepartmentId = Departments.Id", context.Connection);
 
-            InsertCommand = new SqlCommand("Insert Into Employees (FirstName,LastName,DepartmentId) Values (@Name)", context.Connection);
+            InsertCommand = new SqlCommand("Insert Into Employees (FirstName,LastName,DepartmentId) Values (@FirstName,@LastName,(Select Id From Departments Where Name = @Name)); Set @Id = @@IDENTITY;", context.Connection);
+            InsertCommand.Parameters.Add("@FirstName", SqlDbType.NVarChar, 50, "FirstName");
+            InsertCommand.Parameters.Add("@LastName", SqlDbType.NVarChar, 50, "LastName");
             InsertCommand.Parameters.Add("@Name", SqlDbType.NVarChar, 50, "Name");
             InsertCommand.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int, -1, "Id") { Direction = ParameterDirection.Output });
 
-            UpdateCommand = new SqlCommand(@"Update Employees Set Name = @Name", context.Connection);
+            UpdateCommand = new SqlCommand(@"Update Employees Set FirstName = @FirstName, LastName = @LastName, DepartmentId = (Select Id From Departments Where Name = @Name) Where Id = @Id", context.Connection);
+            UpdateCommand.Parameters.Add("@FirstName", SqlDbType.NVarChar, 50, "FirstName");
+            UpdateCommand.Parameters.Add("@LastName", SqlDbType.NVarChar, 50, "LastName");
             UpdateCommand.Parameters.Add("@Name", SqlDbType.NVarChar, 50, "Name");
             UpdateCommand.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int, -1, "Id") { SourceVersion = DataRowVersion.Original });
 
