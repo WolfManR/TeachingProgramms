3fddd0aedc2e8db9db321c52aff2f53138448932
Ivan <wolfmanr@mail.ru>
2019-12-07 12:47:30 +0300

    Revert "Merge branch 'СSharp2' into Lesson8"
    This reverts commit 8d8c9b895faf92a81d32be3f29f25ed98a64d21f.

--- CSharp2/OrganizationProject_ADO_Library/ADO/ADOBase.cs
+++ CSharp2/OrganizationProject_ADO_Library/ADO/ADOBase.cs
@@ -4,11 +4,12 @@ namespace OrganizationProject_ADO_Library.ADO
 {
     public class ADOBase
     {
-        public SqlConnection Connection { get; set; }
-        public SqlDataAdapter Adapter { get; set; } = new SqlDataAdapter();
-        public ADOBase(string connectionString)
+        public SqlConnection Connection { get; }
+        public SqlDataAdapter Adapter { get; }
+        public ADOBase(string connectionString, SqlDataAdapter adapter)
         {
             Connection = new SqlConnection(connectionString);
+            Adapter = adapter;
         }
     }
 }
