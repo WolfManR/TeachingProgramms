40606854f273b0be1e7852a830f12f9d50c9f6a3
Ivan <wolfmanr@mail.ru>
2019-12-08 18:32:15 +0300

    подключил БД через Linq to SQL

---
+++ CSharp3/MailSender_WPFTest/Models/DBclass.cs
@@ -0,0 +1,18 @@
+﻿using MailSender_WPFTest.LinqToSQL;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace MailSender_WPFTest.Models
+{
+    public class DBclass
+    {
+        private EmailsDataContext context = new EmailsDataContext();
+        public ObservableCollection<Emails> Emails { get => new ObservableCollection<Emails>((from c in context.Emails select c).ToList()); }
+        public IQueryable<SMTP> SMTPs { get => from c in context.SMTP select c; }
+
+        void AddEmail()
+        {
+            
+        }
+    }
+}
