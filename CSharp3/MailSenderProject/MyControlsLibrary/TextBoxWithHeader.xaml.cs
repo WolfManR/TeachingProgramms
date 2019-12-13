6125ccbf42e9f975e2812e363002a2de4863779f
Ivan <wolfmanr@mail.ru>
2019-12-13 23:31:13 +0300

    создал проект с финальной версией дизайна приложения для проверки изучаемого материала

---
+++ CSharp3/MailSenderProject/MyControlsLibrary/TextBoxWithHeader.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MyControlsLibrary
+{
+    /// <summary>
+    /// Логика взаимодействия для TextBoxWithHeader.xaml
+    /// </summary>
+    public partial class TextBoxWithHeader : UserControl
+    {
+        public TextBoxWithHeader()
+        {
+            InitializeComponent();
+        }
+        public string Header { get => HeaderControl.Text; set => HeaderControl.Text = value; }
+        public string Text { get => TextBox.Text; set => TextBox.Text = value; }
+    }
+}
