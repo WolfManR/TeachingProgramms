d7d52449684fc044d1223fd941b55ad3b2c109b5
Ivan <wolfmanr@mail.ru>
2019-12-15 00:52:28 +0300

    проект с методичек: добавил проект для тестирования класса SchedulerClass

---
+++ CSharp3/WpfTestMailSenderTests/SchedulerClassTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace WpfTestMailSender.Tests
+{
+    [TestClass()]
+    public class SchedulerClassTests
+    {
+        SchedulerClass sc;
+        TimeSpan ts;
+
+        // Запускается перед стартом каждого тестирующего метода. 
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Debug.WriteLine("Test Initialize");
+            sc = new SchedulerClass();
+            ts = new TimeSpan(); // Возвращаем в случае ошибочно введенного времени
+        }
+
+        [TestMethod()]
+        public void GetSendTime_empty_ts()
+        {
+            string strTimeTest = "";
+            TimeSpan tsTest = sc.GetSendTime(strTimeTest);
+            Assert.AreEqual(ts, tsTest);
+        }
+
+        [TestMethod()]
+        public void GetSendTime_sdf_ts()
+        {
+            string strTimeTest = "sdf";
+            TimeSpan tsTest = sc.GetSendTime(strTimeTest);
+            Assert.AreEqual(ts, tsTest);
+        }
+
+        [TestMethod()]
+        public void GetSendTime_correctTime_Equal()
+        {
+            string strTimeTest = "12:12";
+            TimeSpan tsCorrect = new TimeSpan(12, 12, 0);
+            TimeSpan tsTest = sc.GetSendTime(strTimeTest);
+            Assert.AreEqual(tsCorrect, tsTest);
+        }
+
+        [TestMethod()]
+        public void GetSendTime_inCorrectHour_ts()
+        {
+            string strTimeTest = "25:12";
+            TimeSpan tsTest = sc.GetSendTime(strTimeTest);
+            Assert.AreEqual(ts, tsTest);
+        }
+
+        [TestMethod()]
+        public void GetSendTime_inCorrectMin_ts()
+        {
+            string strTimeTest = "12:65";
+            TimeSpan tsTest = sc.GetSendTime(strTimeTest);
+            Assert.AreEqual(ts, tsTest);
+        }
+
+    }
+}
