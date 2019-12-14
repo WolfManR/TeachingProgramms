6510a71005af13ee54913137027a224ef3e96e6a
Ivan <wolfmanr@mail.ru>
2019-12-15 01:26:08 +0300

    проект с методичек: добавил тест класса SchedulerClass
    пришлось дописать EmailSendServiceClass для работы новых методов SchedulerClass

--- CSharp3/WpfTestMailSender/SchedulerClass.cs
+++ CSharp3/WpfTestMailSender/SchedulerClass.cs
@@ -18,6 +18,19 @@ namespace WpfTestMailSender
         EmailSendServiceClass emailSender;         // экземпляр класса, отвечающего за отправку писем
         DateTime dtSend;                           // дата и время отправки
         ObservableCollection<Email> emails;                  // коллекция email-ов адресатов
+
+        Dictionary<DateTime, string> dicDates = new Dictionary<DateTime, string>();
+        public Dictionary<DateTime, string> DatesEmailTexts
+        {
+            get { return dicDates; }
+            set
+            {
+                dicDates = value;
+                dicDates = dicDates.OrderBy(pair => pair.Key).ToDictionary(pair => pair.Key, pair => pair.Value);
+            }
+        }
+
+
         /// <summary>
         /// Метод, который превращает строку из текстбокса tbTimePicker в TimeSpan
         /// </summary>
@@ -48,17 +61,25 @@ namespace WpfTestMailSender
             timer.Interval = new TimeSpan(0, 0, 1);
             timer.Start();
         }
+
         private void Timer_Tick(object sender, EventArgs e)
         {
-            if (dtSend.ToShortTimeString() == DateTime.Now.ToShortTimeString())
+            if (dicDates.Count == 0)
             {
-                List<string> list = new List<string>();
-                emails.ToList().ForEach(x => list.Add(x.Value));
-                emailSender.SendMails(list);
                 timer.Stop();
                 MessageBox.Show("Письма отправлены.");
             }
-        }
+            else if (dicDates.Keys.First<DateTime>().ToShortTimeString() == DateTime.Now.ToShortTimeString())
+            {
+                emailSender.Body = dicDates[dicDates.Keys.First<DateTime>()];
+                emailSender.Subject = $"Рассылка от {dicDates.Keys.First<DateTime>().ToShortTimeString()} ";
+                List<string> list = new List<string>();
+                emails.ToList().ForEach(x => list.Add(x.Value));
+                emailSender.SendMails(list);
+                dicDates.Remove(dicDates.Keys.First<DateTime>());
+            }
+        }//private void Timer_Tick(object sender, EventArgs e)
+
     }
 
 }
--- CSharp3/WpfTestMailSenderTests/SchedulerClassTests.cs
+++ CSharp3/WpfTestMailSenderTests/SchedulerClassTests.cs
@@ -1,23 +1,64 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace WpfTestMailSender.Tests
 {
     [TestClass()]
     public class SchedulerClassTests
     {
-        SchedulerClass sc;
-        TimeSpan ts;
+        private static SchedulerClass sc;
+        private static TimeSpan ts;
 
         // Запускается перед стартом каждого тестирующего метода. 
-        [TestInitialize]
-        public void TestInitialize()
+        [ClassInitialize]
+        public static void TestInitialize(TestContext context)
         {
             Debug.WriteLine("Test Initialize");
             sc = new SchedulerClass();
             ts = new TimeSpan(); // Возвращаем в случае ошибочно введенного времени
+            sc.DatesEmailTexts = new Dictionary<DateTime, string>()
+            {
+                { new DateTime(2016, 12, 24, 22, 0, 0), "text1" },
+                { new DateTime(2016, 12, 24, 22, 30, 0), "text2" },
+                { new DateTime(2016, 12, 24, 23, 0, 0), "text3" }
+            };
+
         }
+
+        [TestMethod()]
+        public void TimeTick_Dictionare_correct()
+        {
+            DateTime dt1 = new DateTime(2016, 12, 24, 22, 0, 0);
+            DateTime dt2 = new DateTime(2016, 12, 24, 22, 30, 0);
+            DateTime dt3 = new DateTime(2016, 12, 24, 23, 0, 0);
+
+            if (sc.DatesEmailTexts.Keys.First<DateTime>().ToShortTimeString() == dt1.ToShortTimeString())
+            {
+                Debug.WriteLine("Body " + sc.DatesEmailTexts[sc.DatesEmailTexts.Keys.First<DateTime>()]);
+                Debug.WriteLine("Subject " + $"Рассылка от {sc.DatesEmailTexts.Keys.First<DateTime>().ToShortDateString()}  {sc.DatesEmailTexts.Keys.First<DateTime>().ToShortTimeString()}");
+                sc.DatesEmailTexts.Remove(sc.DatesEmailTexts.Keys.First<DateTime>());
+            }
+
+            if (sc.DatesEmailTexts.Keys.First<DateTime>().ToShortTimeString() == dt2.ToShortTimeString())
+            {
+                Debug.WriteLine("Body " + sc.DatesEmailTexts[sc.DatesEmailTexts.Keys.First<DateTime>()]);
+                Debug.WriteLine("Subject " + $"Рассылка от {sc.DatesEmailTexts.Keys.First<DateTime>().ToShortDateString()}  {sc.DatesEmailTexts.Keys.First<DateTime>().ToShortTimeString()}");
+                sc.DatesEmailTexts.Remove(sc.DatesEmailTexts.Keys.First<DateTime>());
+            }
+
+            if (sc.DatesEmailTexts.Keys.First<DateTime>().ToShortTimeString() == dt3.ToShortTimeString())
+            {
+                Debug.WriteLine("Body " + sc.DatesEmailTexts[sc.DatesEmailTexts.Keys.First<DateTime>()]);
+                Debug.WriteLine("Subject " + $"Рассылка от {sc.DatesEmailTexts.Keys.First<DateTime>().ToShortDateString()}  {sc.DatesEmailTexts.Keys.First<DateTime>().ToShortTimeString()}");
+                sc.DatesEmailTexts.Remove(sc.DatesEmailTexts.Keys.First<DateTime>());
+            }
+
+            Assert.AreEqual(0, sc.DatesEmailTexts.Count);
+        }
+
 
         [TestMethod()]
         public void GetSendTime_empty_ts()
