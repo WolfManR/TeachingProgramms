85d415ee51665fcdf9107a6329b58ff4d7cb4f2b
Ivan <wolfmanr@mail.ru>
2019-12-17 15:51:03 +0300

    MailSenderProject: соединил с прошлым проектом

---
+++ CSharp3/MailSenderProject/MailSender/BureauBlue/ViewModel/BureauBlueViewViewModel.cs
@@ -0,0 +1,81 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MailSender.Code;
+using MailSender.Data;
+using MailSender.Data.LinqToSQL;
+using MailSender.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace MailSender.BureauBlue.ViewModel
+{
+    public class BureauBlueViewViewModel : ViewModelBase
+    {
+        public BureauBlueViewViewModel(IDataAccessService dataAccess)
+        {
+            dataService = dataAccess;
+            Emails = dataService.GetEmails();
+            SMTPs = dataService.GetSMTPList();
+        }
+
+        private readonly Window view;
+        private readonly IDataAccessService dataService;
+
+        #region SendMailCmd
+        private RelayCommand<object> sendMailCmd = null;
+        public RelayCommand<object> SendMailCmd => sendMailCmd ?? (sendMailCmd = new RelayCommand<object>(
+            (param) => {
+                var bindings = (object[])param;
+                SendService.SMTPHost = (string)bindings[2];
+                SendService.SMTPPort = int.Parse(bindings[3].ToString());
+                SendService.EnableSSL = (bool)bindings[4];
+                string[] emails = new string[SelectedEmails.Count];
+                for (int i = 0; i < SelectedEmails.Count; i++)
+                {
+                    emails[i] = SelectedEmails[i].Email;
+                }
+                try
+                {
+                    // Find way to take Password correctly
+                    SendService.SendMail((string)bindings[0], ((System.Windows.Controls.PasswordBox)bindings[1]).Password, (string)bindings[5], (string)bindings[6], emails);
+                }
+                catch (Exception ex)
+                {
+                    new ErrorMessage("Невозможно отправить письмо " + "\n" + ex.ToString()) { Owner = view, Title = "Error" }.ShowDialog();
+                }
+
+                new SendEndWindow() { Owner = view }.ShowDialog();
+
+            },
+            param => param != null && SelectedEmails.Count != 0));
+        #endregion
+
+        #region addToSelectedCmd
+        private RelayCommand<Emails> addToSelectedCmd = null;
+        public RelayCommand<Emails> AddToSelectedCmd => addToSelectedCmd ?? (addToSelectedCmd = new RelayCommand<Emails>(
+            (param) => {
+                SelectedEmails.Add(param);
+                Emails.Remove(param);
+            },
+            param => param != null && param is Emails));
+        #endregion
+
+        #region returnFromSelectedCmd
+        private RelayCommand<Emails> returnFromSelectedCmd = null;
+        public RelayCommand<Emails> ReturnFromSelectedCmd => returnFromSelectedCmd ?? (returnFromSelectedCmd = new RelayCommand<Emails>(
+            (param) => {
+                Emails.Add(param);
+                SelectedEmails.Remove(param);
+            },
+            param => param != null && param is Emails));
+        #endregion
+
+        public MailSendService SendService { get; set; } = new MailSendService();
+
+        public ObservableCollection<Emails> Emails { get; set; }
+        public ObservableCollection<Emails> SelectedEmails { get; set; } = new ObservableCollection<Emails>();
+        public List<SMTP> SMTPs { get; set; } 
+    }
+}
---
+++ CSharp3/MailSenderProject/MailSender/Code/MailSendService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace MailSender.Code
+{
+    public class MailSendService
+    {
+        public string SMTPHost { get; set; }
+        public int SMTPPort { get; set; }
+        public bool EnableSSL { get; set; } = true;
+        public bool IsBodyHTML { get; set; } = false;
+
+        public MailSendService() { }
+
+        // изменить на async
+        public void SendMail(string senderEmail, string senderPassword, string title, string letter, params string[] emailsToSend)
+        {
+            foreach (string mail in emailsToSend)
+            {
+                using (MailMessage mm = new MailMessage(senderEmail, mail))
+                {
+                    mm.Subject = title;
+                    mm.Body = letter;
+                    mm.IsBodyHtml = IsBodyHTML;
+
+                    using (SmtpClient sc = new SmtpClient(SMTPHost, SMTPPort))
+                    {
+
+                        sc.EnableSsl = EnableSSL;
+                        sc.Credentials = new NetworkCredential(senderEmail, senderPassword);
+                        try
+                        {
+                            sc.Send(mm);
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
