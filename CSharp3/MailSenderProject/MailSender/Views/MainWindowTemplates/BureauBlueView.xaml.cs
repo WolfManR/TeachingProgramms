8b7c8577b4595644292400d65024bd6497f3ded0
Ivan <wolfmanr@mail.ru>
2019-12-18 14:18:33 +0300

    MailSenderProject: откоректировал каталогизацию проекта, добавил модель приложения

--- CSharp3/MailSenderProject/MailSender/Views/MainWindowTemplates/BureauBlueView.xaml.cs
+++ CSharp3/MailSenderProject/MailSender/Views/MainWindowTemplates/BureauBlueView.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace MailSender.BureauBlue
+namespace MailSender.Views.MainWindowTemplates
 {
     /// <summary>
     /// Логика взаимодействия для BureauBlueView.xaml
--- CSharp3/MailSenderProject/MailSender/Views/MainWindowTemplates/ModernView.xaml.cs
+++ CSharp3/MailSenderProject/MailSender/Views/MainWindowTemplates/ModernView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MailSender.Modern
+namespace MailSender.Views.MainWindowTemplates
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
