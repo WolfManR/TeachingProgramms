88163c78bc0695e67d6b749a0d31d007c916efb8
Tarxos <wolfmanr@mail.ru>
2020-01-16 23:16:27 +0300

    Lesson7/CSVTransferToDB: добавил проект переноса данных из .csv в БД

---
+++ CSharp3/CSVTransferToDB/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSVTransferToDB
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<People> Peoples { get; set; } = new ObservableCollection<People>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog() { Filter = "CSV файлы (*.csv)|*.csv" };
+            if (dialog.ShowDialog() == true)
+            {
+                using (StreamReader sr = new StreamReader(dialog.FileName))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        Peoples.Add(new People { FIO = line[0], Email = line[1], Phone = line[2] });
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSharp3/CSVTransferToDB/People.cs
@@ -0,0 +1,9 @@
+﻿namespace CSVTransferToDB
+{
+    public class People
+    {
+        public string FIO { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
