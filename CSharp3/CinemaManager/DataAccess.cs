016153cc0432ed535e05f5a374d68bc417efc967
Tarxos <wolfmanr@mail.ru>
2020-01-17 20:18:58 +0300

    Lesson7/CinemaManager: добавил DataAccess и FilmShowVM классы

---
+++ CSharp3/CinemaManager/DataAccess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaManager
+{
+    public class DataAccess
+    {
+        CinemaEntitiesContainer context;
+        public DataAccess(CinemaEntitiesContainer container)
+        {
+            context = container;
+        }
+
+        public int AddFilmShow(FilmShow filmShow)
+        {
+            return filmShow.Id;
+        }
+
+        public void UpdateFilmShow(FilmShow filmShow)
+        {
+
+        }
+
+        public void DeleteFilmShow(FilmShow filmShow)
+        {
+
+        }
+    }
+}
---
+++ CSharp3/CinemaManager/FilmShowVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaManager
+{
+    public class FilmShowVM:INotifyPropertyChanged
+    {
+        private FilmShow Film;
+        public FilmShowVM(FilmShow filmShow)
+        {
+            Film = filmShow;
+        }
+        public string FilmShowName 
+        { 
+            get=>Film.Name; 
+            set 
+            {
+                if (Film.Name == value) return;
+                
+            }
+        }
+        public DateTime FilmShowStartTime { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
